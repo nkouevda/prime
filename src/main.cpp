338aaedf47b3b5a68f78a716c741cfd997f99025
Nikita Kouevda <nkouevda@gmail.com>
2013-11-04 01:23:34 -0800

    Use strtol instead of atol; verify conversion.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 // Nikita Kouevda
-// 2013/11/01
+// 2013/11/04
 
 #include <iostream>
 
@@ -8,15 +8,25 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+  int i, n;
+  char *endptr;
+
   if (argc < 2) {
-    cerr << "Usage: prime number ..." << endl;
+    cerr << "usage: prime number ..." << endl;
     return 1;
   }
 
-  for (int i = 1; i < argc; ++i) {
+  for (i = 1; i < argc; ++i) {
+    n = strtol(argv[i], &endptr, 10);
+
+    if (*endptr) {
+      cerr << "prime: illegal argument: " << argv[i] << endl;
+      continue;
+    }
+
     cout << argv[i] << " is";
 
-    if (!isPrime(atol(argv[i]))) {
+    if (!isPrime(n)) {
       cout << " not";
     }
 
