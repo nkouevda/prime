f35cf0da65697a463657b064fd6e7dde5267ba70
Nikita Kouevda <nkouevda@gmail.com>
2013-11-04 01:56:38 -0800

    Add simple help option.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 // 2013/11/04
 
 #include <cstdlib>
+#include <cstring>
 #include <iostream>
 
 #include "prime.hpp"
@@ -11,10 +12,14 @@ using namespace std;
 int main(int argc, char *argv[]) {
   int i, n;
   char *endptr;
+  string usage = "usage: prime [-h] number ...";
 
   if (argc < 2) {
-    cerr << "usage: prime number ..." << endl;
+    cerr << usage << endl;
     return 1;
+  } else if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) {
+    cout << usage << endl;
+    return 0;
   }
 
   for (i = 1; i < argc; ++i) {
