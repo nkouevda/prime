6a4c7f0af62d490ae947c4b3124ab7fb14a13120
Nikita Kouevda <nkouevda@gmail.com>
2014-02-21 20:40:40 -0800

    Use 4 spaces per indent level.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 // Nikita Kouevda
-// 2014/02/18
+// 2014/02/21
 
 #include <stdint.h>
 #include <stdio.h>
@@ -9,33 +9,31 @@
 #include "prime.h"
 
 int main(int argc, char *argv[]) {
-  const char *usage = "usage: prime [-h] number ...";
+    const char *usage = "usage: prime [-h] number ...";
 
-  if (argc < 2) {
-    fprintf(stderr, "%s\n", usage);
+    if (argc < 2) {
+        fprintf(stderr, "%s\n", usage);
+        return 1;
+    } else if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+        printf("%s\n", usage);
+        return 0;
+    }
 
-    return 1;
-  } else if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) {
-    printf("%s\n", usage);
+    char *endptr;
+    int i;
+    uint64_t num;
+
+    for (i = 1; i < argc; ++i) {
+        num = strtoull(argv[i], &endptr, 10);
+
+        if (*endptr) {
+            fprintf(stderr, "prime: illegal argument: %s\n", argv[i]);
+        } else if (is_prime(num)) {
+            printf("%s is prime\n", argv[i]);
+        } else {
+            printf("%s is not prime\n", argv[i]);
+        }
+    }
 
     return 0;
-  }
-
-  char *endptr;
-  int i;
-  uint64_t num;
-
-  for (i = 1; i < argc; ++i) {
-    num = strtoull(argv[i], &endptr, 10);
-
-    if (*endptr) {
-      fprintf(stderr, "prime: illegal argument: %s\n", argv[i]);
-    } else if (is_prime(num)) {
-      printf("%s is prime\n", argv[i]);
-    } else {
-      printf("%s is not prime\n", argv[i]);
-    }
-  }
-
-  return 0;
 }
