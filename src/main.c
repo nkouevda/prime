fa02b015950d2db5dfb9cc806b6ceff3b340b196
Nikita Kouevda <nkouevda@gmail.com>
2014-10-14 21:49:13 -0700

    Switch to 2 space indentation

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 // Nikita Kouevda
-// 2014/05/29
+// 2014/10/14
 
 #include <stdint.h>
 #include <stdio.h>
@@ -9,30 +9,30 @@
 #include "prime.h"
 
 int main(int argc, char *argv[]) {
-    const char *usage = "usage: prime [-h] number ...";
-    char *endptr;
-    uint64_t num;
-    int i;
+  const char *usage = "usage: prime [-h] number ...";
+  char *endptr;
+  uint64_t num;
+  int i;
 
-    if (argc < 2) {
-        fprintf(stderr, "%s\n", usage);
-        return 1;
-    } else if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
-        printf("%s\n", usage);
-        return 0;
-    }
-
-    for (i = 1; i < argc; ++i) {
-        num = strtoull(argv[i], &endptr, 10);
-
-        if (*endptr != '\0') {
-            fprintf(stderr, "prime: illegal argument: %s\n", argv[i]);
-        } else if (is_prime(num)) {
-            printf("%s is prime\n", argv[i]);
-        } else {
-            printf("%s is not prime\n", argv[i]);
-        }
-    }
-
+  if (argc < 2) {
+    fprintf(stderr, "%s\n", usage);
+    return 1;
+  } else if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+    printf("%s\n", usage);
     return 0;
+  }
+
+  for (i = 1; i < argc; ++i) {
+    num = strtoull(argv[i], &endptr, 10);
+
+    if (*endptr != '\0') {
+      fprintf(stderr, "prime: illegal argument: %s\n", argv[i]);
+    } else if (is_prime(num)) {
+      printf("%s is prime\n", argv[i]);
+    } else {
+      printf("%s is not prime\n", argv[i]);
+    }
+  }
+
+  return 0;
 }
--- src/prime.c
+++ src/prime.c
@@ -1,5 +1,5 @@
 // Nikita Kouevda
-// 2014/05/29
+// 2014/10/14
 
 #include <math.h>
 #include <stdint.h>
@@ -7,22 +7,22 @@
 #include "prime.h"
 
 uint8_t is_prime(const uint64_t num) {
-    uint64_t max;
-    uint64_t i;
+  uint64_t max;
+  uint64_t i;
 
-    if (num == 2 || num == 3) {
-        return 1;
-    } else if (num < 2 || num % 2 == 0 || num % 3 == 0) {
-        return 0;
-    }
-
-    max = sqrt(num);
-
-    for (i = 5; i <= max; i += 6) {
-        if (num % i == 0 || num % (i + 2) == 0) {
-            return 0;
-        }
-    }
-
+  if (num == 2 || num == 3) {
     return 1;
+  } else if (num < 2 || num % 2 == 0 || num % 3 == 0) {
+    return 0;
+  }
+
+  max = sqrt(num);
+
+  for (i = 5; i <= max; i += 6) {
+    if (num % i == 0 || num % (i + 2) == 0) {
+      return 0;
+    }
+  }
+
+  return 1;
 }
